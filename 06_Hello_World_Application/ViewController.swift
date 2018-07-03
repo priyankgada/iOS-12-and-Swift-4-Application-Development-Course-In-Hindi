e7978f72b4d626bf9eacefc2f0e281955f9e3e9c
Priyank Gada <kustom.hackers@gmail.com>
2018-07-03 05:42:20 -0700

    Create ViewController.swift

---
+++ 06_Hello_World_Application/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by Priyank Gada on 03/07/18.
+//  Copyright © 2018 Priyank Gada. All rights reserved.
+//
+//www.youtube.com/priyankgada //Execute nai hoga but subscribe karna
+//only because programmers can read
+
+import UIKit //User interface Kit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello Priyank Gada ! Subscribe")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        // Deal with warnings .. fix memory warnings.
+    }
+
+
+}
+
